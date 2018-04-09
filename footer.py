0c9b935be3e2fbb757d56604c97672329f19da45
Koki Yamaguchi <yk49at21@gmail.com>
2018-04-09 11:32:12 +0900

    modified

--- footer.py
+++ footer.py
@@ -3,7 +3,7 @@ def generate():
     <div class="ui inverted vertical footer segment">
         <div class="ui center aligned container">
             <h4 class="ui inverted header">Created by Koki Yamaguchi</h4>
-            <p>Twitter @Ymgch_K</p>
+            <p>Twitter <a href="https://twitter.com/Ymgch_K">@Ymgch_K</a></p>
         </div>
     </div>
 </body>
